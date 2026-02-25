46cd3caf0f4d0587d920650a0d52f361fcffb4eb
Imogen809 <imogenstrong04@gmail.com>
2026-02-25 10:46:54 +0000

    25/02/26 10:46 (sorting data)
    I decided to get rid of asking the user if they wanted to display the course names or not because this isn't really needed, or  is a useful function, and I was getting a bit confused with all my different variables and while loops, it wasn't that readable. This new version still doesn't work, I am still getting errors but I need to pause working on it for now.

--- Assignment-2.cpp
+++ Assignment-2.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<vector>
 #include<cmath>
+#include<algorithm>
 
 using namespace std;
 
@@ -48,10 +49,34 @@ int count_entries(const vector<double>& vals){
     return vals.size();
 }
 
+class Course
+{
+private:
+    string name;
+    double mark;
 
-// int order_entries(array& vals){
+public:
+    Course(string n, double m)
+        : name(n), mark(m) {}
 
-// }
+    string get_name() const
+    {
+        return name;
+    }
+
+    double get_mark() const
+    {
+        return mark;
+    }
+};
+
+
+
+bool order_entries(const Course* a,
+                   const Course* b)
+{
+    return a->get_name() < b->get_name();
+}
 
 int main(){
     //Open file to read and write
@@ -68,37 +93,66 @@ int main(){
 
     vector<double> values;
     vector<string> course_names;
+    vector<Course*> total_course_data; 
+    string course_name;
     double mark;
     int code;
-    string course_name;
 
     // write the marks into a new vector.
     while(my_file >> mark >> code){
-        values.push_back(mark);
-        getline(my_file, course_name);  // discard rest of line
-        course_names.push_back(course_name); // store course name in a separate vector
-
-    }
+    getline(my_file, course_name);  // read rest of line
+    values.push_back(mark);
+    course_names.push_back(course_name);
+    total_course_data.push_back(new Course(course_name, mark)); // <- use course_name, not vector
+}
 
     // counts and prints number of entries in the open file 
     int number_of_entries{count_entries(values)};
     cout<<"Number of entries:"<<number_of_entries<<endl;
 
-        // Ask user if they want course names included
-    string include_courses;
-    cout << "Do you want to include course names when displaying results? (y/n): ";
-    cin >> include_courses;
 
-    if (include_courses == "y" || include_courses == "Y") {
-        cout << "Grades with course names:";
+    string sort_choice;
+    cout << "Do you want to order the marks by course name? (y/n): ";
+    cin >> sort_choice;
+    
+    // Ask if the user wants to sort
+    string sort_total_choice;
+    cout << "Do you want to order the total marks by course name? (y/n): ";
+    cin >> sort_total_choice;
+
+    if (sort_total_choice == "y" || sort_total_choice == "Y") {
+        sort(total_course_data.begin(), total_course_data.end(), order_entries);
+    }
+
+    // Display
+    for (Course* c : total_course_data) {
+        cout << c->get_mark() << " - " << c->get_name() << endl;
+    }
+
+    // Clean up memory
+    for (Course* c : total_course_data) {
+        delete c;
+    }
+
+    if (sort_total_choice == "y" || sort_total_choice == "Y") {
+    sort(total_course_data.begin(), total_course_data.end(), order_entries);
+    cout << "Grades (sorted):";
         for (int i = 0; i < number_of_entries; ++i) {
             cout << values[i] << " - " << course_names[i] << endl;
         }
     }
-
-    // Reset file before we do anything else, as the file pointer is at the end of the file after counting entries, we need to reset it to the beginning of the file before we can read it again
-    my_file.clear();
-    my_file.seekg(0);
+    else if (sort_total_choice == "n" || sort_total_choice == "N") {
+        cout << "Grades:";
+        for (int i = 0; i < number_of_entries; ++i) {
+            cout << values[i] << endl;
+        }
+    }
+    else {
+        cout << "Invalid choice. Displaying unsorted grades." << endl;
+        for (int i = 0; i < number_of_entries; ++i) {
+            cout << values[i] << endl;
+        }
+    }
 
     // calculate and print mean value, the grades are stored in the first column of the file, so we access it using my_file[0]
     double total_stats{stats(values, number_of_entries)};
@@ -111,6 +165,7 @@ int main(){
 
     vector<double> year_values;
     vector<string> year_course_names;
+    vector<Course*> year_data;
     double year_mark;
     int year_code;
     string year_course_name;
@@ -119,6 +174,10 @@ int main(){
     int year;
     cout<<"Enter year (1-4) to get stats for that year:"<<endl;
     cin>>year;
+
+    string year_sort_choice;
+    cout << "Do you want to order the marks by course name? (y/n): ";
+    cin >> year_sort_choice;
 
 
     // ask the user if they want course names included for the year stats
@@ -136,21 +195,43 @@ int main(){
         year_values.push_back(year_mark);
         getline(my_file, year_course_name);
         year_course_names.push_back(year_course_name);
+        year_data.push_back(new Course(year_course_name, year_mark));
     }
 
     getline(my_file, year_course_name);
 }
 
+    if (year_data.empty()){
+    cout << "No data found for year " << year << endl;
+    return 1;
+}
+
+    sort(year_data.begin(), year_data.end(), order_entries);
+
+    for (const Course* c : year_data)
+    {
+        cout << c->get_mark() << " - " << c->get_name() << endl;
+    }
+
+    for (Course* c : year_data)
+        delete c;
+
+
+    if (year_include_courses == "y" || year_include_courses == "Y") {
+    cout << "Grades with course names:";
+    for (const Course* c : year_data) {
+        cout << c->get_mark() << " - "<< c->get_name() << endl;
+    }
+}
+else {
+    cout << "Grades for year " << year << ":";
+    for (const Course* c : year_data) {
+        cout << c->get_mark() << endl;
+    }
+}
 
     // calulate number of entries for that year
     int year_entries{count_entries(year_values)};
-
-    if (year_include_courses == "y" || year_include_courses == "Y") {
-        cout << "Grades with course names:";
-        for (int i = 0; i < year_entries; ++i) {
-            cout << year_values[i] << " - " << year_course_names[i] << endl;
-        }
-    }
 
     // calculate and print mean value, the grades are stored in the first column of the file, so we access it using my_file[0]
     double year_mean_val{mean(year_values, year_entries)};
